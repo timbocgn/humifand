d62bc18420bde668e67ce9e0f9f3251630019fa9
Tim Hagemann <thagemann@Tims-MBP-2013.fritz.box>
2015-03-28 18:38:28 +0100

    Intermediate web-app version. Read config works, Write config just prepared but not working

---
+++ www/read_conf.php
@@ -0,0 +1,65 @@
+<?php
+    
+    ////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //	humifand 	- 	the humidity fan deamon
+    //
+    //	Please check www.way2.net for more information
+    //
+    //  (c) Copyright 2014 by way2.net Services.
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////
+    
+    // --- this is the server side of the AJAX config pages. It is simply an interface to the config files (read part)
+    
+    // --- we will give you JSON data
+    
+    header("content-type: application/json");
+    
+    // --- this is where our conf file is located
+    
+    $confFile = "/etc/humifand.conf";
+    
+    // --- open the file
+    
+    $myfile = fopen($confFile, "r") or die("Unable to open config file!");
+    
+    // --- read line by line
+
+    $hc_array = array();
+    
+    while(!feof($myfile))
+    {
+        // --- read line and remove tailing whitespaces
+        
+        $myline = trim(fgets($myfile));
+        
+        // --- remove comments
+        
+        if (strpos($myline, "#") !== FALSE)
+            $myline = trim(stristr ($myline,"#",true));
+        
+        if (strlen($myline) > 0)
+        {
+            // --- split var and value
+
+            $tokens = explode("=",$myline);
+        
+            if (count($tokens) > 0)
+            {
+                //echo "Key:" . $tokens[0] . "\n";
+                //echo "Value:" . $tokens[1] . "\n";
+                
+                $hc_array[trim($tokens[0])] = trim($tokens[1]);
+            
+            }
+        }
+        
+    }
+    fclose($myfile);
+    
+    // --- we use JSONP just to make our coding life more easy (running th JS code on the dev machine)
+    
+    echo $_GET['callback']. '('. json_encode($hc_array) . ')';
+    
+?>
